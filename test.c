426aab4f6edc1ac8a1b6c6d48f6b1b9c89735ae6
Daan Sprenkels <hello@dsprenkels.com>
2017-10-30 10:38:37 +0100

    Add testing script

---
+++ test.c
@@ -0,0 +1,22 @@
+#include "randombytes.h"
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Generate some random bytes and print them in hex
+    int ret;
+    uint8_t buf[20];
+    size_t i;
+
+    ret = randombytes(buf, sizeof(buf));
+    if (ret != 0) {
+        printf("Error in `randombytes`");
+        return 1;
+    }
+    for (i = 0; i < sizeof(buf); ++i) {
+        printf("%02hhx", buf[i]);
+    }
+    printf("\n");
+    return 0;
+}
