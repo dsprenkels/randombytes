b61993e47e1eff8d97f2c3a356f75945924cf24f
Daan Sprenkels <hello@dsprenkels.com>
2018-03-25 23:54:25 +0200

    Use more robust tests
    These tests are---admittedly---still pretty crappy.
    We now call randombytes twice and check if the output
    differs. This should be better than doing a call to
    randombytes, and not checking anything at all.

--- test.c
+++ test.c
@@ -1,23 +1,36 @@
 #include "randombytes.h"
+
+#include <assert.h>
 #include <errno.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void randombytes_checked(uint8_t *buf, size_t n) {
+	int ret = randombytes(buf, n);
+	if (ret != 0) {
+		printf("Error in `randombytes`: %d\n", errno);
+		abort();
+	}
+}
 
 int main(void)
 {
-    // Generate some random bytes and print them in hex
-    int ret;
-    uint8_t buf[20];
-    size_t i;
+	uint8_t buf1[20] = {0}, buf2[20] = {0};
 
-    ret = randombytes(buf, sizeof(buf));
-    if (ret != 0) {
-        printf("Error in `randombytes`: %d\n", errno);
-        return 1;
-    }
-    for (i = 0; i < sizeof(buf); ++i) {
-        printf("%02hhx", buf[i]);
-    }
-    printf("\n");
-    return 0;
+	randombytes_checked(buf1, sizeof(buf1));
+	assert(memcmp(buf1, buf2, 20) != 0);
+	randombytes_checked(buf2, sizeof(buf2));
+	assert(memcmp(buf1, buf2, 20) != 0);
+
+	for (unsigned int i = 0; i < sizeof(buf1); ++i) {
+		printf("%02hhx", buf1[i]);
+	}
+	printf("\n");
+	for (unsigned int i = 0; i < sizeof(buf2); ++i) {
+		printf("%02hhx", buf2[i]);
+	}
+	printf("\n");
+	return 0;
 }
