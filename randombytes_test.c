74f98202fbde4413bef111dd0a106e0d7b1e0104
Daan Sprenkels <daan@dsprenkels.com>
2021-09-06 21:58:07 +0200

    Add tests for #30
    Fixes #30

--- randombytes_test.c
+++ randombytes_test.c
@@ -7,6 +7,7 @@
 
 static void *current_test = NULL;
 static int syscall_called = 0;
+static int glib_getrandom_called = 0;
 
 // ======== Helper macros and functions ========
 
@@ -45,6 +46,9 @@ static void test_functional(void) {
 	uint8_t buf1[20] = {}, buf2[sizeof(buf1)] = {};
 	const int ret1 = randombytes(buf1, sizeof(buf1));
 	const int ret2 = randombytes(buf2, sizeof(buf2));
+	if (ret1 != 0 || ret2 != 0) {
+		printf("error: %s\n", strerror(errno));
+	}
 	assert(ret1 == 0);
 	assert(ret2 == 0);
 	assert(memcmp(buf1, buf2, sizeof(buf1)) != 0);
@@ -71,6 +75,26 @@ static void test_getrandom_syscall_partial(void) {
 
 static void test_getrandom_syscall_interrupted(void) {
 	syscall_called = 0;
+	uint8_t zero[20] = {};
+	uint8_t buf[sizeof(zero)] = {};
+	const int ret = randombytes(buf, sizeof(buf));
+	assert(ret == 0);
+	assert(memcmp(buf, zero, 20) != 0);
+}
+
+static void test_getrandom_glib_partial(void) {
+	glib_getrandom_called = 0;
+	uint8_t buf[100] = {};
+	const int ret = randombytes(buf, sizeof(buf));
+	assert(ret == 0);
+	assert(glib_getrandom_called >= 5);
+	for (int i = 1; i < 5; i++) {
+		assert(memcmp(&buf[0], &buf[20*i], 20) != 0);
+	}
+}
+
+static void test_getrandom_glib_interrupted(void) {
+	glib_getrandom_called = 0;
 	uint8_t zero[20] = {};
 	uint8_t buf[sizeof(zero)] = {};
 	const int ret = randombytes(buf, sizeof(buf));
@@ -109,7 +133,23 @@ static void test_issue_33(void) {
 
 // ======== Mock OS functions to simulate uncommon behavior ========
 
-#if defined(__linux__) && defined(SYS_getrandom)
+#if defined(__linux__) && defined(__GLIBC__) && ((__GLIBC__ > 2) || (__GLIBC_MINOR__ > 24))
+int __wrap_getrandom(char *buf, size_t buflen, int flags) {
+	glib_getrandom_called++;
+	if (current_test == test_getrandom_glib_partial) {
+		// Fill only 16 bytes, the caller should retry
+		const size_t current_buflen = buflen <= 16 ? buflen : 16;
+		return __real_getrandom(buf, current_buflen, flags);
+	} else if (current_test == test_getrandom_glib_interrupted) {
+		if (glib_getrandom_called < 5) {
+			errno = EINTR;
+			return -1;
+		}
+	}
+	return __real_getrandom(buf, buflen, flags);
+}
+
+#elif defined(__linux__) && defined(SYS_getrandom)
 int __wrap_syscall(int n, char *buf, size_t buflen, int flags) {
 	syscall_called++;
 	if (current_test == test_getrandom_syscall_partial) {
@@ -124,7 +164,7 @@ int __wrap_syscall(int n, char *buf, size_t buflen, int flags) {
 	}
 	return __real_syscall(n, buf, buflen, flags);
 }
-#endif /* defined(__linux__) && defined(SYS_getrandom) */
+#endif /* defined(__linux__) && (defined(SYS_getrandom) or glibc version > 2.24) */
 
 #if defined(__linux__) && !defined(SYS_getrandom)
 int __wrap_ioctl(int fd, int code, int* ret) {
@@ -148,13 +188,19 @@ int main(void) {
 
 	RUN_TEST(test_functional)
 	RUN_TEST(test_empty)
-#if defined(__linux__) && defined(__GLIBC__) && ((__GLIBC__ > 2) || (__GLIBC_MINOR__ > 24))
+#if defined(__linux__) && defined(USE_GLIBC)
+	RUN_TEST(test_getrandom_glib_partial)
+	RUN_TEST(test_getrandom_glib_interrupted)
 	SKIP_TEST(test_getrandom_syscall_partial)
 	SKIP_TEST(test_getrandom_syscall_interrupted)
 #elif defined(__linux__) && defined(SYS_getrandom)
+	SKIP_TEST(test_getrandom_glib_partial)
+	SKIP_TEST(test_getrandom_glib_interrupted)
 	RUN_TEST(test_getrandom_syscall_partial)
 	RUN_TEST(test_getrandom_syscall_interrupted)
 #else
+	SKIP_TEST(test_getrandom_glib_partial)
+	SKIP_TEST(test_getrandom_glib_interrupted)
 	SKIP_TEST(test_getrandom_syscall_partial)
 	SKIP_TEST(test_getrandom_syscall_interrupted)
 #endif /* defined(__linux__) && (defined(SYS_getrandom) or glibc version > 2.24) */
